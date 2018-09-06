6dec5524cb83fb80320a9f5930da0a700f93a969
pangolines <pangolines.ninja@gmail.com>
2018-09-06 05:05:01 -0300

    Commit Inicial

---
+++ src/main/java/ar/com/pangolines/FRANBackend/FranBackendApplication.java
@@ -0,0 +1,12 @@
+package ar.com.pangolines.FRANBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/ar/com/pangolines/FRANBackend/FranBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package ar.com.pangolines.FRANBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FranBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
